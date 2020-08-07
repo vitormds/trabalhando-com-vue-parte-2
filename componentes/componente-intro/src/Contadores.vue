ac3798b1c9bea763de4d2356053a904a91a0d8e6
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-08-07 12:15:16 -0300

    Usando um componente a partir de outro

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,11 @@
+<template> 
+<div class="contadores"> 
+    <app-contador v-for="c in 5" :key="c"></app-contador>
+</div> 
+</template> 
+<script> 
+import Contador from './Contador.vue'
+export default{
+    components:{'app-contador': Contador}
+} 
+</script>
