b58f84f0cb845181d7475eb36db4818cd01b0d16
Vitor Souza <dev_vitorsouza@hotmail.com>
2020-08-07 11:12:50 -0300

    criando componente com cli

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,16 @@
+<template> 
+<div id="app"> {{contador}} 
+</div> 
+</template> 
+<script> 
+export default{
+   data()
+   {
+     return{ contador: 0
+    }
+  }
+} 
+</script> 
+   <style>
+    
+  </style>
